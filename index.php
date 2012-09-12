5d1d827a4d65efb101f579f5a913ccd30f3e8a7a
chunhua <mychunhua@gmail.com>
2012-09-12 18:16:22 +0800

    Hello world commit2

--- index.php
+++ index.php
@@ -1,3 +1,25 @@
-<?php
-echo "Hello, world (from PHP on Windows Azure)";
-?>
+	
+
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/> 
+	</head>
+
+	<title>
+		婚紗照片上傳
+	</title>
+
+	<body>
+
+		<form name = "form" action=http://184.169.163.160/wedding2/login.php method="post" enctype="multipart/form-data">
+			帳戶:<input type="text" name="user_acc" id="user_acc" value = "admin@xx.xx" /><br />
+			密碼:<input type="text" name="user_pwd" id="user_pwd" value = "" /><br />
+			<input type="text" name="self" id="self" value = "1" /><br />
+		<input type="submit" value="submit"/>
+		</form>
+
+
+
+	</body>
+</html>
+
